f729f5dad7e118a8190c12069274bd3f369c2bfc
Максим <m.d.burobin@gmail.com>
2025-10-04 13:35:59 +0300

    chore: remove target folder from version control

--- target/classes/db/migration/004-insert-test-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (username, password, role) VALUES
-('admin', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'ADMIN'),
-('testuser', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'USER');
